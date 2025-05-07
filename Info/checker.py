ae54a2545a900304f90fa3268d5b3d1a28f3c80f
therealpeppy <furghadmanar@gmail.com>
2025-05-07 16:30:31 -0500

    add unit checks

---
+++ Info/checker.py
@@ -0,0 +1,18 @@
+with open("./Info/Pokemon.csv", 'r') as pokeFile:
+	errs = []
+	passed = 0
+	total = 1026
+	for lineNum, lineContent in pokeFile.readlines():
+		if len(lineContent.split(",")) != 13:
+			if len(lineContent.split(",")) < 13:
+				errs.append("Missing data")
+			if len(lineContent.split(",")) < 13:
+				errs.append("Unkown data found")
+
+		if errs.__len__ != 0:
+			print(f"❌ {lineNum}:")
+			for msg in errs:
+				print(msg)
+		else:
+			print(f"✅ {lineNum}: All checks passed! ({passed}/{total})")
+			passed+=1
