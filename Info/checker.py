0a0c50f11ee0b56433e82c56ea985f5d7d1ace63
therealpeppy <furghadmanar@gmail.com>
2025-05-07 21:57:53 -0500

    add art checker

--- Info/checker.py
+++ Info/checker.py
@@ -1,13 +1,18 @@
+import os
+
 with open("./Info/Pokemon.csv", 'r') as pokeFile:
-	errs = []
 	passed = 0
 	total = 1025
 	for lineNum, lineContent in enumerate(pokeFile.readlines()):
+		errs = []
 		if len(lineContent.split(",")) != 13:
 			if len(lineContent.split(",")) < 13:
 				errs.append("Missing data")
 			if len(lineContent.split(",")) < 13:
 				errs.append("Unkown data found")
+
+		elif not os.path.exists(f"./Info/Art/{lineContent.split(",")[0].strip("\"")}.png"):
+			errs.append(f"Image not found for id {lineContent.split(",")[0].strip("\"")}")
 
 		if len(errs) != 0:
 			print(f"❌ {lineNum}:")
