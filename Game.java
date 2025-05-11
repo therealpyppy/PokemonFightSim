479b3bdae5b42c5c7ec1966f86ff2ce0df793f37
therealpeppy <furghadmanar@gmail.com>
2025-05-10 22:54:12 -0500

    Make pokemon list class for reuse

--- Game.java
+++ Game.java
@@ -1,18 +1,11 @@
 import java.awt.*;
-import java.io.*;
 import javax.swing.*;
-import java.util.List;
-import java.util.ArrayList;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public class Game {
 	private int screenWidth;
 	private int screenHeight;
-	
-	private List<Object[]> originalPokemonData = new ArrayList<>();
-	private DefaultTableModel model;
 	
 	public Game(int width, int height) {
 		this.screenWidth = width;
@@ -37,99 +30,26 @@ public class Game {
 		searchBar.setBounds((screenWidth - searchBar.getPreferredSize().width) / 2, 40, searchBar.getPreferredSize().width, searchBar.getPreferredSize().height);
 		pokemonSelection.add(searchBar);
 		
-		try (BufferedReader br = new BufferedReader(new FileReader("./Info/Pokemon.csv"))) {
-			String line = br.readLine();
-			
-			while ((line = br.readLine()) != null) {
-				String[] parts = line.split(",");
-				String id = parts[0].replace("\"", "");
-				String name = parts[1].replace("\"", "");
-				String fullName = name + "    ||    " + id;
-				
-				String imgPath = "./Info/Art/" + String.format("%03d", Integer.parseInt(id)) + ".png";
-				ImageIcon rawIcon = new ImageIcon(imgPath);
-				Image rawImage = rawIcon.getImage();
-				Image scaledImage = getScaledImage(rawImage, 80);
-				ImageIcon icon = new ImageIcon(scaledImage);
-				
-				Object[] row = {icon, fullName};
-				originalPokemonData.add(row);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		String[] columnNames = {"Image", "Name || ID"};
-		
-		DefaultTableModel model1 = new DefaultTableModel(columnNames, 0) {
-			public Class<?> getColumnClass(int column) {
-				return (column == 0) ? ImageIcon.class : String.class;
-			}
-			public boolean isCellEditable(int row, int column) {
-				return false;
-			}
-		};
-		
-		JTable table1 = new JTable(model1);
-		table1.setRowHeight(80);
-		table1.getColumnModel().getColumn(0).setPreferredWidth(100);
-		table1.getColumnModel().getColumn(1).setPreferredWidth(screenWidth - 350);
-		table1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table1.getTableHeader().setResizingAllowed(false);
-		table1.getTableHeader().setReorderingAllowed(false);
+		PokemonList PokemonList1 = new PokemonList(screenWidth, screenHeight, new Rectangle(15, 70, screenWidth / 2 - 30 - 16, screenHeight - 90));
+		JScrollPane scroll1 = PokemonList1.getScroll();
 
-		DefaultTableModel model2 = new DefaultTableModel(columnNames, 0) {
-			public Class<?> getColumnClass(int column) {
-				return (column == 0) ? ImageIcon.class : String.class;
-			}
-			public boolean isCellEditable(int row, int column) {
-				return false;
-			}
-		};
-		
-		JTable table2 = new JTable(model2);
-		table2.setRowHeight(80);
-		table2.getColumnModel().getColumn(0).setPreferredWidth(100);
-		table2.getColumnModel().getColumn(1).setPreferredWidth(screenWidth - 350);
-		table2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table2.getTableHeader().setResizingAllowed(false);
-		table2.getTableHeader().setReorderingAllowed(false);
-
-		for (Object[] row : originalPokemonData) {
-			model1.addRow(row);
-			model2.addRow(row);
-		}
-		
-		JScrollPane scroll1 = new JScrollPane(table1);
-		scroll1.setBounds(15, 70, screenWidth / 2 - 30 - 16, screenHeight - 90);
-		
-		JScrollPane scroll2 = new JScrollPane(table2);
-		scroll2.setBounds(screenWidth / 2 + 15 + 16, 70, screenWidth - (screenWidth / 2 + 15) - 15 - 16, screenHeight - 90);
-		
-		scroll1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scroll1.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-		scroll2.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scroll2.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		PokemonList PokemonList2 = new PokemonList(screenWidth, screenHeight, new Rectangle(screenWidth / 2 + 15 + 16, 70, screenWidth - (screenWidth / 2 + 15) - 15 - 16, screenHeight - 90));
+		JScrollPane scroll2 = PokemonList2.getScroll();
 
 		pokemonSelection.add(scroll1);
 		pokemonSelection.add(scroll2);
 		
-		for (Object[] row : originalPokemonData) {
-			model1.addRow(row);
-		}
-		
 		searchBar.getDocument().addDocumentListener(new DocumentListener() {
 			public void insertUpdate(DocumentEvent e) {
-				search(searchBar.getText());
+				PokemonList1.search(searchBar.getText());
 			}
 			
 			public void removeUpdate(DocumentEvent e) {
-				search(searchBar.getText());
+				PokemonList1.search(searchBar.getText());
 			}
 			
 			public void changedUpdate(DocumentEvent e) {
-				search(searchBar.getText());
+				PokemonList1.search(searchBar.getText());
 			}
 		});
 		
@@ -139,44 +59,6 @@ public class Game {
 		pokemonSelection.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		f.setContentPane(pokemonSelection);
 		f.pack();
-	}
-
-	private Image getScaledImage(Image srcImg, int targetHeight) {
-		int width = srcImg.getWidth(null);
-		int height = srcImg.getHeight(null);
-	
-		if (width <= 0 || height <= 0) return srcImg;
-	
-		double scale = (double) targetHeight / height;
-		int newWidth = (int) (width * scale);
-	
-		return srcImg.getScaledInstance(newWidth, targetHeight, Image.SCALE_SMOOTH);
-	}
-	
-
-	private void search(String text) {
-		text = text.toLowerCase();
-		model.setRowCount(0);
-		
-		boolean found = false;
-		for (Object[] row : originalPokemonData) {
-			String full = (String) row[1];
-			String name = full.split("\\|\\|")[0].trim().toLowerCase();
-			if (name.contains(text)) {
-				model.addRow(row);
-				found = true;
-			}
-		}
-		
-		if (!found) {
-			for (Object[] row : originalPokemonData) {
-				String full = (String) row[1];
-				String id = full.split("\\|\\|")[1].trim().toLowerCase();
-				if (id.contains(text)) {
-					model.addRow(row);
-				}
-			}
-		}
 	}
 	
 	public static void main(String[] args) {
---
+++ PokemonList.java
@@ -0,0 +1,118 @@
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
+public class PokemonList {
+	private List<Object[]> originalPokemonData = new ArrayList<>();
+	private DefaultTableModel model;
+	private int screenWidth;
+	//private int screenHeight;
+	private Rectangle position;
+	private JTable table;
+
+	public PokemonList(int screenWidth, int screenHeight, Rectangle position) {
+		this.screenWidth = screenWidth;
+		//this.screenHeight = screenHeight;
+		this.position = position;
+
+		String[] columnNames = {"Image", "Name || ID"};
+		
+		this.model = new DefaultTableModel(columnNames, 0) {
+			public Class<?> getColumnClass(int column) {
+				return (column == 0) ? ImageIcon.class : String.class;
+			}
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+	}
+
+	public JScrollPane getScroll() {
+		try (BufferedReader br = new BufferedReader(new FileReader("./Info/Pokemon.csv"))) {
+			String line = br.readLine();
+				
+			while ((line = br.readLine()) != null) {
+				String[] parts = line.split(",");
+				String id = parts[0].replace("\"", "");
+				String name = parts[1].replace("\"", "");
+				String fullName = name + "    ||    " + id;
+				
+				String imgPath = "./Info/Art/" + String.format("%03d", Integer.parseInt(id)) + ".png";
+				ImageIcon rawIcon = new ImageIcon(imgPath);
+				Image rawImage = rawIcon.getImage();
+				Image scaledImage = getScaledImage(rawImage, 80);
+				ImageIcon icon = new ImageIcon(scaledImage);
+				
+				Object[] row = {icon, fullName};
+				originalPokemonData.add(row);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		this.table = new JTable(this.model);
+		this.table.setRowHeight(80);
+		this.table.getColumnModel().getColumn(0).setPreferredWidth(100);
+		this.table.getColumnModel().getColumn(1).setPreferredWidth(this.screenWidth - 350);
+		this.table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.table.getTableHeader().setResizingAllowed(false);
+		this.table.getTableHeader().setReorderingAllowed(false);
+
+		for (Object[] row : originalPokemonData) {
+			this.model.addRow(row);
+		}
+		
+		JScrollPane scroll = new JScrollPane(this.table);
+		scroll.setBounds(this.position);
+
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		for (Object[] row : originalPokemonData) {
+			this.model.addRow(row);
+		}
+
+		return scroll;
+	}
+
+	public void search(String text) {
+		text = text.toLowerCase();
+		this.model.setRowCount(0);
+		
+		boolean found = false;
+		for (Object[] row : originalPokemonData) {
+			String full = (String) row[1];
+			String name = full.split("\\|\\|")[0].trim().toLowerCase();
+			if (name.contains(text)) {
+				this.model.addRow(row);
+				found = true;
+			}
+		}
+		
+		if (!found) {
+			for (Object[] row : originalPokemonData) {
+				String full = (String) row[1];
+				String id = full.split("\\|\\|")[1].trim().toLowerCase();
+				if (id.contains(text)) {
+					this.model.addRow(row);
+				}
+			}
+		}
+	}
+
+	private Image getScaledImage(Image srcImg, int targetHeight) {
+		int width = srcImg.getWidth(null);
+		int height = srcImg.getHeight(null);
+	
+		if (width <= 0 || height <= 0) return srcImg;
+	
+		double scale = (double) targetHeight / height;
+		int newWidth = (int) (width * scale);
+	
+		return srcImg.getScaledInstance(newWidth, targetHeight, Image.SCALE_SMOOTH);
+	}
+}
